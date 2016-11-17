f0160cfb4d824832b00afc3e210a36d51c93931c
TVMKISHORE <kishore.thadiboyina@gmail.com>
2016-11-17 20:32:16 +0530

    Comments added

--- Imputation.R
+++ Imputation.R
@@ -8,6 +8,7 @@
 #missForest
 install.packages("missForest")
 library(missForest)
+install.packages("mice")
 ##-------------------------------------------------------------##
 #install package and load library
 install.packages("Hmisc")
@@ -79,17 +80,69 @@ test=impute(test,7,impute_arg$imputed$Speciesvirginica,Catogorical='True')
 
 
 
-#--------------------------------------------
-#Lookup function_
+
+
+#-------------------------------------
+# Imputation with mice package
+#-------------------------------------
+#https://www.r-bloggers.com/imputing-missing-data-with-r-mice-package/
+#-------------------------------------
+library(missForest)
+library(Hmisc)
+#load data
+data("iris")
+# prepare a data set to impute
+#seed missing values ( 10% )
+iris.mis <- prodNA(iris, noNA = 0.1)
+summary(iris.mis)
+
+#subsetting complete and !complete data
+iris.mis[complete.cases(iris.mis),]
+iris.mis[!complete.cases(iris.mis),]
+
+library(mice)
+md.pattern(iris.mis)
+
+tempData <- mice(iris.mis,m=5,maxit=50,meth='pmm',seed=500) #meth='pmm'
+summary(tempData)
+
+#Features imputes are below. sice m=5 , there are five imputed datasets
+tempData$imp$Sepal.Width
+tempData$imp$Sepal.Length
+tempData$imp$Petal.Length
+tempData$imp$Petal.Width
+
+#to see the method used
+tempData$meth
+
+completedData <- complete(tempData,1)
+
+
+
+#----------------------------------------------------------------------------------------
+#some useful functions
+#----------------------------------------------------------------------------------------
+#df1,--- Base Data frame 
+#df2,--- Data frame to look
+#what_tolook-- What to look for a match
+#what_toget----What to get finally (suffix with .x or .y depending on the columns to get)
+#Lookup function
 lookup_table <- function(df1,df2,what_tolook,what_toget){
-               temp_df = merge(df1,df2,by=what_tolook)
-               ret_df  = temp_df[what_toget]
-               return(ret_df)
+  temp_df = merge(df1,df2,by=what_tolook)
+  ret_df  = temp_df[what_toget]
+  return(ret_df)
 }
 
 
+
 #--------------------------------------------
-#function to get the imputed data to main dataset
+#Function to replace the Na cell of data frame with multiple imputed datasets
+#The function finds mean or mode of multiple imputed data sets, and replaces NAs
+#--------------------------------------------
+#df1--Data frame
+#col--Col number
+#imputed_data---impued data contains single column from multiple datasets(imputed data sets)
+#--------------------------------------------
 impute <-  function(df1,col,imputed_data,Catogorical='True') {
   for (i in 1:nrow(df1)) {
     if (is.na(df1[i,col])){  
@@ -99,8 +152,10 @@ impute <-  function(df1,col,imputed_data,Catogorical='True') {
           if (Catogorical=='True'){
             rep_val<-getmode(imputed_data[j,])
             df1[i,col]<-rep_val
+            print(rep_val)
             break
           }else{rep_val<-mean(imputed_data[j,]) 
+          print('test')
           df1[i,col]<-rep_val
           print(i)
           print(rep_val)  
@@ -124,3 +179,5 @@ getmode <- function(v) {
 #-----------------------------------------------------------
 #To delete a column
 df[[2]]<- NULL   # will delete the secod column
+
+
